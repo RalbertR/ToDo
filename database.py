9c6af8b3462effcbb4471f3ae08247cb8a5a7a01
RalbertR <raulrobino@gmail.com>
2025-07-05 22:41:36 -0300

    Base de datos de Produccion cambiada

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:Kodak2025@localhost/TodoApplicationDatabase'
+SQLALCHEMY_DATABASE_URL = 'postgresql://tareas_database_user:1FnooQ1aGawfvF8NabTVhimlJ1SphKc6@dpg-d1kt6p3e5dus73f07gug-a.oregon-postgres.render.com/tareas_database'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
